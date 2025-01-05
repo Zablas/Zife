4b15151e74b03e2a4ff414bca9fa66be5e858cef
Karolis Balčiūnas <vagabaliukas@gmail.com>
2025-01-05 13:43:58 +0200

    Setup a window

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,19 @@
 const std = @import("std");
+const rl = @import("raylib");
+const constants = @import("constants");
+const colors = constants.colors;
 
-pub fn main() !void {}
+pub fn main() !void {
+    rl.initWindow(750, 750, "Zife");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(12);
+    rl.setExitKey(rl.KeyboardKey.null);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(colors.grey);
+    }
+}
